0442dce3170ddc17638dcbb6ac59b1256c2e8ea5
Joel Campos <underkillr@gmail.com>
2020-12-21 13:44:35 -0600

    Final curso Vue basico

---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl">Cryto Exchange v1.0.0</h1>
+    <p>
+      Proyecto para obtener las cotizaciones de las cryptomonedas más
+      importantes a traves de coincap <br />
+      Este proyecto es utilizado en el curso de vue.js básico de platzi.com
+    </p>
+    <router-link to="/" class="mt-5 text-xl text-green-600 hover:underline">
+      Volver a la página de inicio
+    </router-link>
+
+    <br />
+
+    <p>
+      Programado por <a class="mt-5 text-xl text-blue-600 hover:underline" href="https://github.com/camposjoel">Joel Campos</a> 🇲🇽
+    </p>
+  </div>
+</template>
